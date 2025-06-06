ae5b2c5298e91f8257bffc0b18e8f4b7511b1c6c
jecheung9 <jeffcheung424@gmail.com>
2025-06-06 07:12:50 -0700

    backend authentication

---
+++ packages/backend/src/CredentialsProvider.ts
@@ -0,0 +1,59 @@
+import { Collection, MongoClient } from "mongodb";
+import bcrypt from "bcrypt";
+
+interface ICredentialsDocument {
+    _id: string;
+    username: string;
+    password: string;
+}
+
+export class CredentialsProvider {
+    private readonly collection: Collection<ICredentialsDocument>;
+    private readonly mongoClient: MongoClient;
+
+    constructor(mongoClient: MongoClient) {
+        this.mongoClient = mongoClient;
+        const COLLECTION_NAME = process.env.CREDS_COLLECTION_NAME;
+        if (!COLLECTION_NAME) {
+            throw new Error("Missing CREDS_COLLECTION_NAME from env file");
+        }
+        this.collection = mongoClient.db().collection<ICredentialsDocument>(COLLECTION_NAME);
+    }
+
+    async registerUser(username: string, plaintextPassword: string) {
+        const checkUser = await this.collection.findOne({ username });
+        if (checkUser) {
+            return false;
+        }
+        const salt = await bcrypt.genSalt(10);
+        console.log("Salt:", salt);
+        const hashedPassword = await bcrypt.hash(plaintextPassword, salt);
+        console.log("Hash:", hashedPassword);
+        await this.collection.insertOne({
+            _id: username,
+            username: username,
+            password: hashedPassword
+        });
+
+        const usersCollectionName = process.env.USERS_COLLECTION_NAME;
+        if (!usersCollectionName) {
+            throw new Error("Missing USERS_COLLECTION_NAME from env file");
+        }
+        const usersCollection = this.mongoClient.db().collection<{ _id: string; username: string; }>(usersCollectionName);
+        await usersCollection.insertOne({
+            _id: username,
+            username,
+        });
+
+        return true;
+    }
+
+    async verifyPassword(username: string, plaintextPassword: string) {
+        const user = await this.collection.findOne({ username });
+        if (!user) {
+            return false;
+        }
+        const hashedDatabasePassword = user.password; 
+        return await bcrypt.compare(plaintextPassword, hashedDatabasePassword);
+    }
+}
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,6 +4,8 @@ import { ValidRoutes } from "./shared/ValidRoutes";
 import { connectMongo } from "./connectMongo";
 import { EventProvider } from "./EventProvider";
 import { registerEventRoutes } from "./routes/eventRoutes";
+import { registerAuthRoutes, verifyAuthToken } from "./routes/authRoutes";
+import { CredentialsProvider } from "./CredentialsProvider";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
@@ -11,13 +13,19 @@ const STATIC_DIR = process.env.STATIC_DIR || "public";
 
 const mongoClient = connectMongo();
 const eventProvider = new EventProvider(mongoClient);
+const credentialsProvider = new CredentialsProvider(mongoClient);
 
 const app = express();
 app.use(express.json()) // middleware
 
+app.locals.JWT_SECRET = process.env.JWT_SECRET;
+
 app.use(express.static(STATIC_DIR));
 
+
+app.use("/api/*", verifyAuthToken);
 registerEventRoutes(app, eventProvider);
+registerAuthRoutes(app, credentialsProvider);
 
 app.get(Object.values(ValidRoutes), (req: Request, res: Response) => {
     res.sendFile("index.html", {root: STATIC_DIR})
